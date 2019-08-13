9e26df6024b0b16bdd6834250a8b17e7a7a0e35c
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2019-08-13 14:46:06 -0300

    Add URI Online Judge Problem 1004

---
+++ uri-online-judge/python3/1004.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1004
+
+
+def main():
+    A, B = int(input()), int(input())
+
+    PROD = A * B
+
+    print("PROD =", PROD)
+
+
+# Start the execution if it's the main script
+if __name__ == "__main__":
+    main()
