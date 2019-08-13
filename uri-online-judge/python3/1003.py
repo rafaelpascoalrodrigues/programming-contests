7e020d0fc7b5bede6a359a456e620d57aec8757a
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2019-08-13 14:44:06 -0300

    Add URI Online Judge Problem 1003

---
+++ uri-online-judge/python3/1003.py
@@ -0,0 +1,15 @@
+#!/bin/python3
+# https://www.urionlinejudge.com.br/judge/en/problems/view/1003
+
+
+def main():
+    A, B = int(input()), int(input())
+
+    SOMA = A + B
+
+    print("SOMA =", SOMA)
+
+
+# Start the execution if it's the main script
+if __name__ == "__main__":
+    main()
