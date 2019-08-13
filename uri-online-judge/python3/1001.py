5fc58962ef74d8fbec264012fc17c32347f26cfc
Rafael Rodrigues <rafaelpascoalrodrigues@gmail.com>
2019-08-13 14:31:59 -0300

    Add URI Online Judge Problem 1001

---
+++ uri-online-judge/python3/1001.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+
+def main():
+    A, B = int(input()), int(input())
+
+    X = A + B
+
+    print("X =", X)
+
+
+# Start the execution if it's the main script
+if __name__ == "__main__":
+    main()
